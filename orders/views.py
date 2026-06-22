416f61c5b164a78716e382baba04b549e3636fdd
RohanEkke <rohanekke5@gmail.com>
2026-06-23 02:05:03 +0530

    worked on frontend - orders list, details and created chatbox

---
+++ orders/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Order, RefundRequest
+from django.contrib.auth.decorators import login_required
+
+# Create your views here.
+
+
+@login_required
+def order_list(request):
+    orders = Order.objects.filter(user=request.user)
+    print(orders)
+    context = {
+        'orders' : orders
+    }
+    return render(request, 'orders_list.html', context)
+
+
+def order_detail(request, order_id):
+    order = get_object_or_404(Order, id=order_id, user=request.user)
+
+    # get refund history for order
+    refunds = RefundRequest.objects.filter(order=order)
+    context = {
+        'order' : order,
+        'refunds' : refunds
+    }
+
+    return render(request, 'order_detail.html', context)
